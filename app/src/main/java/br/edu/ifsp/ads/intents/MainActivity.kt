08dd2f547b402c9ff3d62459dc901f7cf8cbb5bc
NathanHermes <hermesnathan8@gmail.com>
2023-03-28 10:35:38 -0300

    feat: Create finally function

--- app/src/main/java/br/edu/ifsp/ads/intents/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/ads/intents/MainActivity.kt
@@ -1,15 +1,18 @@
 package br.edu.ifsp.ads.intents
 
+import android.Manifest.permission.CALL_PHONE
 import android.content.Intent
+import android.content.Intent.*
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.os.Environment.DIRECTORY_PICTURES
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import br.edu.ifsp.ads.intents.databinding.ActivityMainBinding
@@ -20,33 +23,46 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object Constantes {
-        const val PARAMETRO_EXTRA = "PARAMETRO_EXTRA"
+        const val EXTRA_PARAMS = "EXTRA_PARAMS"
     }
 
     private lateinit var parl: ActivityResultLauncher<Intent>
-    private lateinit var permissaoChamadaArl: ActivityResultLauncher<String>
+    private lateinit var callPermissionArl: ActivityResultLauncher<String>
+    private lateinit var getImageArl: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
         parl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result?.resultCode == RESULT_OK) {
-                val retorno: String = result.data?.getStringExtra(PARAMETRO_EXTRA) ?: ""
-                amb.parametroTv.text = retorno
+                val response: String = result.data?.getStringExtra(EXTRA_PARAMS) ?: ""
+                amb.paramsTv.text = response
             }
         }
 
         amb.entrarParametroBt.setOnClickListener {
-            val parametroIntent = Intent("PALMEIRAS_CAMPEAO_PAULISTA_ACTION")
-            parametroIntent.putExtra(PARAMETRO_EXTRA, amb.parametroTv.text.toString())
-            parl.launch(parametroIntent)
+            val paramIntent = Intent("PALMEIRAS_CAMPEAO_PAULISTA_ACTION")
+            paramIntent.putExtra(EXTRA_PARAMS, amb.paramsTv.text.toString())
+            parl.launch(paramIntent)
         }
 
-        permissaoChamadaArl = registerForActivityResult(ActivityResultContracts.RequestPermission()) {permissaoConcedida ->
-            if (permissaoConcedida) {
-
-            } else {
+        callPermissionArl = registerForActivityResult(ActivityResultContracts.RequestPermission()) { grantedPermission ->
+            if (grantedPermission) callNumber(true)
+            else {
                 Toast.makeText(this, "Permissão necessária para continuar", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+
+        getImageArl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
+            if (result.resultCode == RESULT_OK) {
+                val imageUri = result.data?.data
+                imageUri?.let {
+                    amb.paramsTv.text = it.toString()
+                    val toViewImage = Intent(ACTION_VIEW, it)
+                    startActivity(toViewImage)
+                }
             }
         }
     }
@@ -59,25 +75,52 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.viewMi -> {
-                val url: Uri = Uri.parse(amb.parametroTv.text.toString())
-                val navegadorIntent = Intent(Intent.ACTION_VIEW, url)
-                startActivity(navegadorIntent)
+                val url: Uri = Uri.parse(amb.paramsTv.text.toString())
+                val browserIntent = Intent(ACTION_VIEW, url)
+                startActivity(browserIntent)
                 true
             }
             R.id.callMi -> {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-
-                } else {
-
-                }
-
+                    if (checkSelfPermission(CALL_PHONE) == PERMISSION_GRANTED) callNumber(true)
+                    else callPermissionArl.launch(CALL_PHONE)
+                } else callNumber(true)
                 true
             }
-            R.id.dialMi -> true
-            R.id.pickMi -> true
-            R.id.chooserMi -> true
+            R.id.dialMi -> {
+                callNumber(false)
+                true
+            }
+            R.id.pickMi -> {
+                val getImageIntent = Intent(ACTION_PICK)
+                val imageDirectory = Environment
+                    .getExternalStoragePublicDirectory(DIRECTORY_PICTURES)
+                    .path
+
+                getImageIntent.setDataAndType(Uri.parse(imageDirectory), "image/*")
+                getImageArl.launch(getImageIntent)
+                true
+            }
+            R.id.chooserMi -> {
+                val url: Uri = Uri.parse(amb.paramsTv.text.toString())
+                val browserIntent = Intent(ACTION_VIEW, url)
+
+                val selectAppIntent = Intent(ACTION_CHOOSER)
+                selectAppIntent.putExtra(EXTRA_TITLE, "Escolha seu navegador preferido")
+                selectAppIntent.putExtra(EXTRA_INTENT, browserIntent)
+
+                startActivity(selectAppIntent)
+                true
+            }
             else -> false
         }
+    }
+
+    private fun callNumber (call: Boolean) {
+        val numberUri: Uri = Uri.parse("tel: ${amb.paramsTv.text}")
+        val callIntent = Intent(if (call) ACTION_CALL else ACTION_DIAL)
+        callIntent.data = numberUri
+        startActivity(callIntent)
     }
 
     /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
--- app/src/main/java/br/edu/ifsp/ads/intents/ParametroActivity.kt
+++ app/src/main/java/br/edu/ifsp/ads/intents/ParametroActivity.kt
@@ -3,7 +3,7 @@ package br.edu.ifsp.ads.intents
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.edu.ifsp.ads.intents.MainActivity.Constantes.PARAMETRO_EXTRA
+import br.edu.ifsp.ads.intents.MainActivity.Constantes.EXTRA_PARAMS
 import br.edu.ifsp.ads.intents.databinding.ActivityParametroBinding
 
 class ParametroActivity : AppCompatActivity() {
@@ -15,14 +15,14 @@ class ParametroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(apb.root)
 
-        intent.getStringExtra(PARAMETRO_EXTRA)?.let { parametro ->
+        intent.getStringExtra(EXTRA_PARAMS)?.let { parametro ->
             apb.parametroEt.setText(parametro)
         }
 
         apb.enviarParametroBt.setOnClickListener {
             val parametro: String = apb.parametroEt.text.toString()
             val retornoIntent = Intent()
-            retornoIntent.putExtra(PARAMETRO_EXTRA, parametro)
+            retornoIntent.putExtra(EXTRA_PARAMS, parametro)
             setResult(RESULT_OK, retornoIntent)
             finish()
         }
